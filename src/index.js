9bf1b00c54adff12874b5c9abf25cc049a73148a
pradnyesh patil <patilpradnyesh1312@gmail.com>
2021-08-24 00:00:17 +0530

    Create React Quiz App

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
+
+class QuizApp extends Component {
+  state = {
+    quizQuestions: [],
+    score: 0,
+    responses: 0
+  };
+  
+  getQuestions = () => {
+    quizService().then(question => {
+      this.setState({
+        quizQuestions: question
+      });
+    });
+  };
+  checkAnswer(answer, correctAnswer){
+    if(answer === correctAnswer){
+      this.setState({
+        score: this.state.score + 1
+      })
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+    })
+  }
+
+  playAgain = () => {
+    this.getQuestions();
+    this.setState({
+      score: 0,
+      responses: 0,
+    })
+  }
+  componentDidMount(){
+    this.getQuestions();
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="title">QuizApp</div>
+        {this.state.quizQuestions.length > 0 && 
+          this.state.responses < 5 &&
+            this.state.quizQuestions.map(
+            ({question, answers, correct, questionId}) => (
+              <QuestionBox 
+                question={question} 
+                options={answers} 
+                key={questionId}
+                selected={ answer => this.checkAnswer(answer, correct)}
+              />
+            )
+          )}
+        {this.state.responses === 5 ? (
+          <Result score={this.state.score} playAgain={this.playAgain}/> 
+        ) : null }
+      </div>
+    );
+  };
+}
+
+ReactDOM.render(
+  <QuizApp/>, document.getElementById("root")
+);
